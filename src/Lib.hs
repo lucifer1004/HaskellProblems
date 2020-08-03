b5c644c782f32884adedbb21ffa058b6d8fcc2b9
Gabriel Wu <qqbbnease1004@126.com>
2020-08-04 07:06:38 +0800

    1-20

---
+++ src/Lib.hs
@@ -0,0 +1,144 @@
+module Lib where
+
+-- Custom types
+data NestedList a = Elem a | List [NestedList a]
+
+data Elem a = Multiple Int a | Single a deriving (Eq, Show)
+
+-- Problem 01
+myLast :: [a] -> Maybe a
+myLast []       = Nothing
+myLast [x     ] = Just x
+myLast (_ : xs) = myLast xs
+
+-- Problem 02
+myButLast :: [a] -> Maybe a
+myButLast []       = Nothing
+myButLast (x : xs) = case length xs of
+  0 -> Nothing
+  1 -> Just x
+  _ -> myButLast xs
+
+-- Problem 03
+elementAt :: [a] -> Int -> Maybe a
+elementAt xs n = if n <= 0 || n > length xs
+  then Nothing
+  else if n == 1 then Just $ head xs else elementAt (tail xs) $ n - 1
+
+-- Problem 04
+myLength :: [a] -> Int
+myLength []       = 0
+myLength (x : xs) = 1 + myLength xs
+
+-- Problem 05
+myReverse :: [a] -> [a]
+myReverse []       = []
+myReverse (x : xs) = myReverse xs ++ [x]
+
+-- Problem 06
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome x = x == reverse x
+
+-- Problem 07
+flatten :: NestedList a -> [a]
+flatten (Elem x ) = [x]
+flatten (List xs) = concatMap flatten xs
+
+-- Problem 08
+compress :: Eq a => [a] -> [a]
+compress []       = []
+compress [x     ] = [x]
+compress (x : xs) = if x == head y then y else [x] ++ y where y = compress xs
+
+-- Problem 09
+pack :: Eq a => [a] -> [[a]]
+pack []       = []
+pack [x     ] = [[x]]
+pack (x : xs) = if x == head (head y)
+  then [head y ++ [x]] ++ tail y
+  else [[x]] ++ y
+  where y = pack xs
+
+-- Problem 10
+encode :: Eq a => [a] -> [(Int, a)]
+encode []       = []
+encode [x     ] = [(1, x)]
+encode (x : xs) = if x == h then [(hc + 1, h)] ++ t else [(1, x)] ++ y
+ where
+  y       = encode xs
+  (hc, h) = head y
+  t       = tail y
+
+-- Problem 11
+tupleToElem :: (Int, a) -> Elem a
+tupleToElem (1, x) = Single x
+tupleToElem (n, x) = Multiple n x
+
+encodeModified :: Eq a => [a] -> [Elem a]
+encodeModified = (map tupleToElem) . encode
+
+-- Problem 12
+elemToTuple :: Elem a -> (Int, a)
+elemToTuple (Single x    ) = (1, x)
+elemToTuple (Multiple n x) = (n, x)
+
+tupleToList :: (Int, a) -> [a]
+tupleToList (0, x) = []
+tupleToList (n, x) = [x] ++ tupleToList (n - 1, x)
+
+decodeModified :: [Elem a] -> [a]
+decodeModified xs = concatMap (tupleToList . elemToTuple) xs
+
+-- Problem 13
+encodeDirect :: Eq a => [a] -> [Elem a]
+encodeDirect []       = []
+encodeDirect [x     ] = [Single x]
+encodeDirect (x : xs) = if x == h
+  then [Multiple (num + 1) x] ++ t
+  else [Single x] ++ y
+ where
+  y        = encodeDirect xs
+  (num, h) = case head y of
+    Multiple n0 h0 -> (n0, h0)
+    Single h0      -> (1, h0)
+  t = tail y
+
+-- Problem 14
+dupli :: [a] -> [a]
+dupli []       = []
+dupli (x : xs) = [x, x] ++ dupli xs
+
+-- Problem 15
+repli :: [a] -> Int -> [a]
+repli []       _ = []
+repli [x     ] n = if n <= 0 then [] else [x] ++ repli [x] (n - 1)
+repli (x : xs) n = (repli [x] n) ++ (repli xs n)
+
+-- Problem 16
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery xs n =
+  if n <= 0 then [] else (take (n - 1) xs) ++ (dropEvery (drop n xs) n)
+
+-- Problem 17
+split :: [a] -> Int -> ([a], [a])
+split []       _ = ([], [])
+split (x : xs) n = if n <= 0 then ([], x : xs) else ([x] ++ h, t)
+  where (h, t) = split xs (n - 1)
+
+-- Problem 18
+slice :: [a] -> Int -> Int -> [a]
+slice xs l r = drop (l - 1) (take r xs)
+
+-- Problem 19
+rotate :: [a] -> Int -> [a]
+rotate [] _ = []
+rotate xs d =
+  let n = length xs
+  in  if d < 0 || d >= n
+        then rotate xs $ rem ((rem d n) + n) n
+        else slice xs (1 + d) n ++ slice xs 1 d
+
+-- Problem 20
+removeAt :: [a] -> Int -> (Maybe a, [a])
+removeAt xs n = (elementAt xs n, take (n - 1) xs ++ drop n xs)
---
+++ test/Spec.hs
@@ -0,0 +1,207 @@
+import           Data.List
+import           Data.Ord
+import           Lib
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.SmallCheck         as SC
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [scProps]
+
+unitTests :: TestTree
+unitTests = testGroup
+  "Unit tests"
+  [ ut01
+  , ut02
+  , ut03
+  , ut06
+  , ut07
+  , ut08
+  , ut09
+  , ut10
+  , ut11
+  , ut12
+  , ut13
+  , ut14
+  , ut15
+  , ut16
+  , ut17
+  , ut18
+  , ut19
+  , ut20
+  ]
+
+scProps :: TestTree
+scProps = testGroup
+  "(checked by SmallCheck)"
+  [ SC.testProperty "Problem 04 : myLength"
+    $ \list -> myLength (list :: [Int]) == length list
+  , SC.testProperty "Problem 05 : myReverse"
+    $ \list -> myReverse (list :: [Int]) == reverse list
+  , SC.testProperty "Problem 11 & 13: encode"
+    $ \list -> encodeModified (list :: [Int]) == encodeDirect list
+  ]
+
+ut01 :: TestTree
+ut01 = testGroup
+  "Problem 01: myLast"
+  [ testCase "Length >= 1" $ myLast [1, 2, 3] @?= Just 3
+  , testCase "Empty list" $ myLast [] @?= (Nothing :: Maybe Int)
+  ]
+
+ut02 :: TestTree
+ut02 = testGroup
+  "Problem 02: myButLast"
+  [ testCase "Length >= 2" $ myButLast [1, 2, 3] @?= Just 2
+  , testCase "Single element" $ myButLast [1] @?= (Nothing :: Maybe Int)
+  , testCase "Empty list" $ myButLast [] @?= (Nothing :: Maybe Int)
+  ]
+
+ut03 :: TestTree
+ut03 = testGroup
+  "Problem 03: elementAt"
+  [ testCase "Valid index" $ elementAt [1, 2, 3] 2 @?= Just 2
+  , testCase "Invalid index" $ elementAt [1, 2] 3 @?= (Nothing :: Maybe Int)
+  ]
+
+ut06 :: TestTree
+ut06 = testGroup
+  "Problem 06: isPalindrome"
+  [ testCase "Empty list" $ isPalindrome "" @?= True
+  , testCase "Single element" $ isPalindrome [1] @?= True
+  , testCase "Multiple elements, palindrome" $ isPalindrome "ABCBA" @?= True
+  , testCase "Multiple elements, non-palindrome"
+  $   isPalindrome [1, 2, 2, 3]
+  @?= False
+  ]
+
+ut07 :: TestTree
+ut07 = testGroup
+  "Problem 07: flatten"
+  [ testCase "Single element" $ flatten (Elem 1) @?= [1]
+  , testCase "Nested list"
+  $ flatten (List [List [List [Elem 1, Elem 2], Elem 3], List [Elem 4, Elem 5]])
+  @?= [1, 2, 3, 4, 5]
+  ]
+
+ut08 :: TestTree
+ut08 = testGroup
+  "Problem 08: compress"
+  [ testCase "Non-empty list" $ compress "aabbccaaad" @?= "abcad"
+  , testCase "Empty list" $ compress [] @?= ([] :: [Int])
+  ]
+
+ut09 :: TestTree
+ut09 = testGroup
+  "Problem 09: pack"
+  [ testCase "Non-empty list"
+  $   pack [1, 1, 2, 2, 3, 4]
+  @?= [[1, 1], [2, 2], [3], [4]]
+  , testCase "Empty list" $ pack "" @?= []
+  ]
+
+ut10 :: TestTree
+ut10 = testGroup
+  "Problem 10: encode"
+  [ testCase "Non-empty list"
+  $   encode "aaabbc"
+  @?= [(3, 'a'), (2, 'b'), (1, 'c')]
+  , testCase "Empty list" $ encode [] @?= ([] :: [(Int, Int)])
+  ]
+
+ut11 :: TestTree
+ut11 = testGroup
+  "Problem 11: encodeModified"
+  [ testCase "Non-empty list"
+  $   encodeModified "aaabbc"
+  @?= [Multiple 3 'a', Multiple 2 'b', Single 'c']
+  , testCase "Empty list" $ encodeModified [] @?= ([] :: [Elem Int])
+  ]
+
+ut12 :: TestTree
+ut12 = testGroup
+  "Problem 12: decodeModified"
+  [ testCase "Non-empty list"
+  $   decodeModified [Multiple 3 3, Single 1, Multiple 2 2]
+  @?= [3, 3, 3, 1, 2, 2]
+  , testCase "Empty list" $ decodeModified [] @?= ([] :: [Int])
+  ]
+
+ut13 :: TestTree
+ut13 = testGroup
+  "Problem 13: encodeDirect"
+  [ testCase "Non-empty list"
+  $   encodeDirect "aaabbc"
+  @?= [Multiple 3 'a', Multiple 2 'b', Single 'c']
+  , testCase "Empty list" $ encodeDirect [] @?= ([] :: [Elem Int])
+  ]
+
+ut14 :: TestTree
+ut14 = testGroup
+  "Problem 14: dupli"
+  [ testCase "Empty list" $ dupli [] @?= ([] :: [Int])
+  , testCase "Non-empty list" $ dupli "abc" @?= "aabbcc"
+  ]
+
+ut15 :: TestTree
+ut15 = testGroup
+  "Problem 15: repli"
+  [ testCase "Empty list" $ repli [] 3 @?= ([] :: [Int])
+  , testCase "Non-empty list" $ repli "abc" 3 @?= "aaabbbccc"
+  ]
+
+ut16 :: TestTree
+ut16 = testGroup
+  "Problem 16: dropEvery"
+  [ testCase "Normal drop" $ dropEvery [1, 2, 3, 4] 2 @?= [1, 3]
+  , testCase "Large drop" $ dropEvery [1, 2, 3, 4] 5 @?= [1, 2, 3, 4]
+  , testCase "Negative drop" $ dropEvery [1, 2, 3, 4] (-1) @?= ([] :: [Int])
+  ]
+
+ut17 :: TestTree
+ut17 = testGroup
+  "Problem 17: split"
+  [ testCase "Normal split" $ split [1, 2, 3, 4, 5] 2 @?= ([1, 2], [3, 4, 5])
+  , testCase "Large split" $ split [1, 2, 3, 4, 5] 6 @?= ([1, 2, 3, 4, 5], [])
+  , testCase "Zero split" $ split [1, 2, 3, 4, 5] 0 @?= ([], [1, 2, 3, 4, 5])
+  , testCase "Empty list" $ split [] 0 @?= ([] :: [Int], [] :: [Int])
+  ]
+
+ut18 :: TestTree
+ut18 = testGroup
+  "Problem 18: slice"
+  [ testCase "Normal slice" $ slice [1, 2, 3, 4, 5] 2 4 @?= [2, 3, 4]
+  , testCase "Left overflow" $ slice [1, 2, 3, 4, 5] (-1) 2 @?= [1, 2]
+  , testCase "Right overflow" $ slice [1, 2, 3, 4, 5] 4 7 @?= [4, 5]
+  , testCase "Left over right" $ slice [1, 2, 3, 4, 5] 4 3 @?= ([] :: [Int])
+  , testCase "Empty list" $ slice [] 1 2 @?= ([] :: [Int])
+  ]
+
+ut19 :: TestTree
+ut19 = testGroup
+  "Problem 19: rotate"
+  [ testCase "Left rotate" $ rotate [1, 2, 3, 4, 5] 2 @?= [3, 4, 5, 1, 2]
+  , testCase "Right rotate" $ rotate [1, 2, 3, 4, 5] (-1) @?= [5, 1, 2, 3, 4]
+  , testCase "Empty list" $ rotate [] 1 @?= ([] :: [Int])
+  ]
+
+ut20 :: TestTree
+ut20 = testGroup
+  "Problem 20: removeAt"
+  [ testCase "Normal removal"
+  $   removeAt [1, 2, 3, 4, 5] 2
+  @?= (Just 2, [1, 3, 4, 5])
+  , testCase "Left overflow"
+  $   removeAt [1, 2, 3, 4, 5] 0
+  @?= (Nothing, [1, 2, 3, 4, 5])
+  , testCase "Right overflow"
+  $   removeAt [1, 2, 3, 4, 5] 6
+  @?= (Nothing, [1, 2, 3, 4, 5])
+  , testCase "Empty list" $ removeAt [] 0 @?= (Nothing, [] :: [Int])
+  ]
